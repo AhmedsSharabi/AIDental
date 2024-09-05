62e1188566ce2dce9adeca173772b83c5cbbf296
Ahmed Sharabi <ahmeds.alsharabi@gmail.com>
2024-09-05 23:32:15 +0800

    Initial Commit

---
+++ AIDental/AIDentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIDentalApp.swift
+//  AIDental
+//
+//  Created by Ahmed Sharabi on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AIDentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
